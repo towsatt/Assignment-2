f91bade86114071906df71aeb6017a0d87a0c8bc
towsatt <leonardo.bilato03@gmail.com>
2024-05-20 21:57:06 +0200

    close #5 implementazione romanprinter

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -1,5 +1,35 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
+    public static String printAsciiArt(String roman) {
+        String res = "";
 
+        for (int i = 0; i < getNumber('I').length; i++) {
+            for (int j = 0; j < roman.length(); j++) {
+                res += getNumber(roman.charAt(j))[i];
+            }
+            res += "\n";
+        }
+
+        return res;
+    }
+
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    public static String[] getNumber(char ch) {
+        switch (ch) {
+            case 'I': return "  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|".split("\n");
+            case 'V': return " __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    ".split("\n");
+            case 'X': return " __   __\n \\ \\ / /\n  \\ V / \n   > <  \n  / . \\ \n /_/ \\_\\".split("\n");
+            case 'L': return "  _      \n | |     \n | |     \n | |     \n | |____ \n |______|".split("\n");
+            case 'C': return "   _____ \n  / ____|\n | |     \n | |     \n | |____ \n  \\_____|".split("\n");
+            case 'D': return "  _____  \n |  __ \\ \n | |  | |\n | |  | |\n | |__| |\n |_____/ ".split("\n");
+            case 'M': return "  __  __ \n |  \\/  |\n | \\  / |\n | |\\/| |\n | |  | |\n |_|  |_|".split("\n");
+            default:
+                System.err.println(ch);
+                throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,14 +1,88 @@
 package it.unipd.mtss;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class RomanPrinterTest {
+    static RomanPrinter printer;
+
     @Test
-    public void testPrint() {
-        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
-        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
-        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
-	}
+    public void testPrintI() {
+        int arabic_number = 1;
+        String expected = "  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n";
+
+        String ascii_art_for_I = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_I);
+    }
+
+    @Test
+    public void testPrintV() {
+        int arabic_number = 5;
+        String expected = " __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    \n";
+
+        String ascii_art_for_V = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_V);
+    }
+
+    @Test
+    public void testPrintX() {
+        int arabic_number = 10;
+        String expected = " __   __\n \\ \\ / /\n  \\ V / \n   > <  \n  / . \\ \n /_/ \\_\\\n";
+
+        String ascii_art_for_X = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_X);
+    }
+
+    @Test
+    public void testPrintL() {
+        int arabic_number = 50;
+        String expected = "  _      \n | |     \n | |     \n | |     \n | |____ \n |______|\n";
+
+        String ascii_art_for_L = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_L);
+    }
+
+    @Test
+    public void testPrintC() {
+        int arabic_number = 100;
+        String expected = "   _____ \n  / ____|\n | |     \n | |     \n | |____ \n  \\_____|\n";
+
+        String ascii_art_for_C = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_C);
+    }
+
+    @Test
+    public void testPrintD() {
+        int arabic_number = 500;
+        String expected = "  _____  \n |  __ \\ \n | |  | |\n | |  | |\n | |__| |\n |_____/ \n";
+
+        String ascii_art_for_D = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_D);
+    }
+
+    @Test
+    public void testPrintM() {
+        int arabic_number = 1000;
+        String expected = "  __  __ \n |  \\/  |\n | \\  / |\n | |\\/| |\n | |  | |\n |_|  |_|\n";
+
+        String ascii_art_for_M = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_M);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNumberZeroAsciiArt() {
+        int arabic_number = 0;
+        String expected = "";
+
+        String ascii_art_for_zero = RomanPrinter.print(arabic_number);
+
+        assertEquals(expected, ascii_art_for_zero);
+    }
 }
